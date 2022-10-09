3aa67bb623a3125346fb54a874baae602c8b69e2
Jakub Tracz <jakub.tracz.1994@o2.pl>
2022-10-09 20:26:14 +0200

    End of Section 2

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,30 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers;
+
+public class ActivitiesController : BaseController
+{
+    private readonly DataContext _context;
+
+    public ActivitiesController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Activity>>> GetActivities() => await _context.Activities.ToListAsync();
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Activity>> GetActivities(Guid id)
+    {
+        var result = await _context.Activities.FindAsync(id);
+
+        if (result is null)
+            return NotFound(id);
+
+        return result;
+    }
+}
---
+++ API/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+
+services.AddControllers();
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+services.AddDbContext<DataContext>(optionsBuilder =>
+{
+    optionsBuilder.UseSqlite(builder.Configuration.GetConnectionString("Reactivities"));
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+
+try
+{
+    var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedData(context);
+}
+catch (Exception e)
+{
+    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+    logger.LogError(e,"An error occured during db creation");
+    throw;
+}
+
+await app.RunAsync();
